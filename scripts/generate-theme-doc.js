d51caf876a7040750212cc983d40797abd905b9c
Rawan Abdo <abdo_rawan@hotmail.com>
2022-08-25 20:42:24 +1000

    Update generate-theme-doc.js

--- scripts/generate-theme-doc.js
+++ scripts/generate-theme-doc.js
@@ -17,7 +17,7 @@ With inbuilt themes, you can customize the look of the card without doing any ma
 Use \`?theme=THEME_NAME\` parameter like so :-
 
 \`\`\`md
-![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=rawan-abdo&theme=dark&show_icons=true)
+![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=rawan-abdo&count_private=true&theme=dark&show_icons=true)
 \`\`\`
 
 ## Stats
